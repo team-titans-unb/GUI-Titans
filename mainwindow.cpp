f8d3fac5c83c9f9d0f504b263199acce8f940cfb
Guilherme Mendes <guioliveira20082009@gmail.com>
2025-08-10 20:36:14 -0300

    Adiciona código inicial do projeto GUI-Titans

---
+++ mainwindow.cpp
@@ -0,0 +1,84 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QSettings>
+#include <QApplication>
+#include <QIcon>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // Restaura o texto da categoria
+    QSettings settings("Titans", "Categoria");
+    QString ultimoTexto = settings.value("ultimoTextoCategoria", "VSSS").toString();
+    ui->ButtonCategoria->setText(ultimoTexto);
+
+    // Inicializa botão como PLAY
+    setButtonToPlay();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_ButtonCategoria_clicked()
+{
+    QString novoTexto;
+    if (ui->ButtonCategoria->text() == "VSSS") {
+        novoTexto = "SSL";
+    } else {
+        novoTexto = "VSSS";
+    }
+    ui->ButtonCategoria->setText(novoTexto);
+
+    QSettings settings("Titans", "Categoria");
+    settings.setValue("ultimoTextoCategoria", novoTexto);
+}
+
+void MainWindow::on_ButtonPlayPause_clicked()
+{
+    if (!playing) {
+
+        setButtonToPause();
+        QApplication::processEvents();
+
+        // startCommunication();
+
+        playing = true;
+    } else {
+
+        setButtonToPlay();
+        QApplication::processEvents();
+
+
+        // stopCommunication();
+
+        playing = false;
+    }
+}
+
+void MainWindow::setButtonToPlay()
+{
+    ui->ButtonPlayPause->setText("");
+    ui->ButtonPlayPause->setIcon(QIcon(":/icons/play.svg"));
+    ui->ButtonPlayPause->setIconSize(QSize(32, 32));
+    ui->ButtonPlayPause->setStyleSheet(
+        "QPushButton { background:#4CAF50; border-radius:6px; padding:10px; }"
+        "QPushButton:pressed { background:#45A049; }"
+        );
+}
+
+void MainWindow::setButtonToPause()
+{
+    ui->ButtonPlayPause->setText("");
+    ui->ButtonPlayPause->setIcon(QIcon(":/icons/pause.svg"));
+    ui->ButtonPlayPause->setIconSize(QSize(32, 32));
+    ui->ButtonPlayPause->setStyleSheet(
+        "QPushButton { background:#F44336; border-radius:6px; padding:10px; }"
+        "QPushButton:pressed { background:#D32F2F; }"
+        );
+}
+
