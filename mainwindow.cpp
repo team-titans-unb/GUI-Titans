75927c7ac8c5e5052b0dcf279a4189a133be9b5c
Donnk61 <luis.arruda@gmail.com>
2025-08-11 15:47:49 -0400

    #4 Selecionar Lado

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QSettings>
-#include <QApplication>
-#include <QIcon>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,13 +8,11 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Restaura o texto da categoria
     QSettings settings("Titans", "Categoria");
-    QString ultimoTexto = settings.value("ultimoTextoCategoria", "VSSS").toString();
-    ui->ButtonCategoria->setText(ultimoTexto);
 
-    // Inicializa botão como PLAY
-    setButtonToPlay();
+    QString ultimoTexto = settings.value("ultimoTextoCategoria", "VSSS").toString();
+
+    ui->ButtonCategoria->setText(ultimoTexto);
 }
 
 MainWindow::~MainWindow()
@@ -27,71 +23,28 @@ MainWindow::~MainWindow()
 void MainWindow::on_ButtonCategoria_clicked()
 {
     QString novoTexto;
-    if (ui->ButtonCategoria->text() == "VSSS") {
+
+
+    if(ui->ButtonCategoria->text()=="VSSS"){
         novoTexto = "SSL";
-    } else {
+    }else
         novoTexto = "VSSS";
-    }
+
     ui->ButtonCategoria->setText(novoTexto);
 
     QSettings settings("Titans", "Categoria");
+
     settings.setValue("ultimoTextoCategoria", novoTexto);
 }
 
-void MainWindow::on_ButtonPlayPause_clicked()
+
+void MainWindow::on_ButtonLado_clicked()
 {
-    if (!playing) {
-
-        setButtonToPause();
-        QApplication::processEvents();
-
-        // startCommunication();
-
-        playing = true;
-    } else {
-
-        setButtonToPlay();
-        QApplication::processEvents();
-
-
-        // stopCommunication();
-
-        playing = false;
+    if(ui->ButtonLado->text()=="ESQUERDO"){
+        ui->ButtonLado->setText("DIRETO");
+    }
+    else{
+        ui->ButtonLado->setText("ESQUERDO!");
     }
 }
 
-void MainWindow::setButtonToPlay()
-{
-    ui->ButtonPlayPause->setText("");
-    ui->ButtonPlayPause->setIcon(QIcon(":/icons/play.svg"));
-    ui->ButtonPlayPause->setIconSize(QSize(32, 32));
-    ui->ButtonPlayPause->setStyleSheet(
-        "QPushButton { background:#4CAF50; border-radius:6px; padding:10px; }"
-        "QPushButton:pressed { background:#45A049; }"
-        );
-}
-
-void MainWindow::setButtonToPause()
-{
-    ui->ButtonPlayPause->setText("");
-    ui->ButtonPlayPause->setIcon(QIcon(":/icons/pause.svg"));
-    ui->ButtonPlayPause->setIconSize(QSize(32, 32));
-    ui->ButtonPlayPause->setStyleSheet(
-        "QPushButton { background:#F44336; border-radius:6px; padding:10px; }"
-        "QPushButton:pressed { background:#D32F2F; }"
-        );
-}
-
-void MainWindow::on_ButtonCor_clicked()
-{
-    if (isButtonColorBlue) {        
-        ui->ButtonCor->setStyleSheet("background-color: yellow;");
-        ui->ButtonCor->setText("Amarelo");
-        isButtonColorBlue = false;
-    } 
-    else {
-        ui->ButtonCor->setStyleSheet("background-color: #00bbff;");
-        ui->ButtonCor->setText("Azul");
-        isButtonColorBlue = true;
-    }
-}
